a7ebe9b5e8149857067baa2ffd8b0e039c18ea8b
alex-galushka <agalushka@svitla.com>
2012-04-06 10:22:30 +0300

    youtube video

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 //= require_tree .
 
 $(document).ready(function() {
-  $('a[rel*=lazybox]').lazybox({close: false, modal: false, opacity: 0.7, klass: 'lazybox'});
+  $('a[rel*=lazybox]').lazybox({close: false, modal: false, opacity: 0.7, klass: 'lazybox', speed: 200});
 
   $.lazybox.settings = {cancelClass: "button green", submitClass: 'button red', closeImg: false, niceClose: false}
   $.rails.allowAction = $.lazybox.confirm;
