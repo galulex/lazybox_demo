8c0b32a2a7040e95ef0496b882dd8c5c05760d51
galulex <galulex@gmail.com>
2013-09-20 11:50:47 +0300

    localized link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
       content_tag(:span, error.is_a?(Array) ? error.first : error, :class => :error)
     end
   end
+
+  def llink_to(name = nil, options = nil, html_options = nil, &block)
+    name = t("links.#{name}") unless block_given?
+    link_to name, options, html_options, &block
+  end
+
 end
