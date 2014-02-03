6b18465e6c2f16c0ebf846048b0ad45df9c38901
galulex <galulex@gmail.com>
2014-02-03 19:16:01 +0200

    Fix images on heroku

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.0.0'
+ruby '1.9.3'
 gem 'rails', '4.0.2'
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
@@ -14,6 +14,7 @@ gem 'jbuilder', '~> 1.2'
 gem 'thin'
 
 group :production do
+  gem 'rails_12factor'
   gem 'pg'
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ LazyboxDemo::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
